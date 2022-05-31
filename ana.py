4573a608494a46c218e8d2b9980fcf30c48d10c1
Lisaojet <105356867+Lisaojet@users.noreply.github.com>
2022-05-31 15:37:39 +0100

    Update ana.py

--- ana.py
+++ ana.py
@@ -4,7 +4,7 @@ sorted_str1 = sorted(str1)
 sorted_str2 = sorted(str2)
 
 if sorted_str1 == sorted_str2:
-    print("This is an Anagram")
+    return True
 else:
-     print("This is not an Anagram")
+     return False
     
