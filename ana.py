523002c0bf003209e4b4d9628d48042d244d313e
Lisaojet <105356867+Lisaojet@users.noreply.github.com>
2022-05-31 15:43:04 +0100

    Update ana.py

--- ana.py
+++ ana.py
@@ -1,10 +1,11 @@
-str1 = input("word:")
-str2 = input("anagram:")
-sorted_str1 = sorted(str1)
-sorted_str2 = sorted(str2)
+dev find_anagram(word, anagram)
+    str1 = input("word:")
+    str2 = input("anagram:")
+    sorted_str1 = sorted(str1)
+    sorted_str2 = sorted(str2)
 
 if sorted_str1 == sorted_str2:
     return True
 else:
-     return False
-    
+     return False 
+print(find_anagram("below", "elbow"))
